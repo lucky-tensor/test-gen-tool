1e9776027ae62e8284eb0515d4437ee0f1ad9cb2
lucky-tensor <77554843+lucky-tensor@users.noreply.github.com>
2023-04-17 16:05:48 -0400

    testsetting up clap

---
+++ genesis-tools/src/main.rs
@@ -0,0 +1,33 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct GenesisCliArgs {
+
+    #[command(subcommand)]
+    command: Option<Sub>,
+}
+
+#[derive(Subcommand)]
+enum Sub {
+    /// does testing things
+    Fork {
+        /// lists test values
+        #[arg(short, long)]
+        test_mode: bool,
+    },
+}
+
+fn main() {
+    let cli = GenesisCliArgs::parse();
+    match cli.command {
+        Some(Sub::Fork { test_mode }) => {
+          dbg!(&test_mode);
+        },
+        _ => {},
+    }
+
+
+
+    // Continued program logic goes here...
+}
